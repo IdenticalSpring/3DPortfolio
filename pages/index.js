e4c8f22ba520f55f0e7526a46a7324333a7f9137
IdenticalSpring <thanhxuan8054@gmail.com>
2025-04-03 03:10:47 +0700

    1

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,8 @@ const Page = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  margin: 0;
+  padding: 0;
   width: 100%;
   min-height: 100vh;
   background-color: #1937d6;
