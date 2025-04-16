3f9641e8fef868bc26ce753dcc58c3596d6bbceb
IdenticalSpring <thanhxuan8054@gmail.com>
2025-04-17 00:13:06 +0700

    update

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,36 @@
 // pages/_document.js
-import { Html, Head, Main, NextScript } from "next/document";
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <div id="portal-root"></div>
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    // Create an instance of ServerStyleSheet
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      // Run the rendering method with the style sheet instance
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      // Extract the initial props that may include the styles
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        // Inject the extracted styles into the head
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      // Seal the stylesheet to prevent memory leaks
+      sheet.seal();
+    }
+  }
 }
